7b05455e88ce30bf1f1f52f26f56796b40a93c07
Stanley Phu <sphu@ucsd.edu>
2016-05-19 20:00:18 -0700

    feat(platform): App bar and drawer implemented with Materials-UI, replaces the previous burger-menu
    The old burger menu has now been replaced with a new app bar and drawer implemented using
    Materials-UI to match the rest of the app.

--- app/imports/ui/AuthPage.jsx
+++ app/imports/ui/AuthPage.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const AuthPage = ({ content, link }) => (
   <div className="page-auth">
+    <div className="container">
     <div className="page-content">
       {content}
       {link}
+    </div>
     </div>
   </div>
 );
--- app/imports/ui/Menu.jsx
+++ app/imports/ui/Menu.jsx
@@ -1,90 +1,161 @@
 import React, { Component, PropTypes } from 'react';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import AppBar from 'material-ui/AppBar';
+
 import { Link } from 'react-router';
 
-import FaList from 'react-icons/lib/fa/list';
-import FaCalendarCheckO from 'react-icons/lib/fa/calendar-check-o';
-import FaFacebookOfficial from 'react-icons/lib/fa/facebook-official';
-import FaSignOut from 'react-icons/lib/fa/sign-out';
-import FaSignIn from 'react-icons/lib/fa/sign-in';
-import FaChild from 'react-icons/lib/fa/child';
-
-let BurgerMenu = require('react-burger-menu').slide;
-
 export default class Menu extends Component {
+
   constructor(props) {
     super(props);
-
     this.state = {
-      isOpen: false,
+      open: false,
     };
   }
 
-  renderLoggedIn() {
-    const { user, logout } = this.props;
-    const email = user.emails[0].address;
-
-    return (
-      <div class="bm-menu">
-        <BurgerMenu class="bm-item-list" isOpen={this.state.isOpen}>
-          <Link id="tasks" to="/">
-            <FaList />
-            <span>Tasks</span>
-          </Link>
-          <Link id="calendar" to="/calendar">
-            <FaCalendarCheckO />
-            <span>Calendar</span>
-          </Link>
-          <Link id="social" to="/social">
-            <FaFacebookOfficial />
-            <span>Social</span>
-          </Link>
-          <span id="user-email">Signed in as:<br/>{email}</span>
-          <Link id="logout" to="/" onClick={logout}>
-            <FaSignOut />
-            <span>Logout</span>
-          </Link>
-        </BurgerMenu>
-      </div>
+  //handleToggle = () => this.setState({open: !this.state.open});
+  handleToggle() {
+    this.setState(
+      {open: !this.state.open}
     );
   }
 
-  renderLoggedOut() {
-    return (
-      <div class="bm-menu">
-        <BurgerMenu class="bm-item-list" isOpen={this.state.isOpen}>
-          <Link id="tasks" to="/">
-            <FaList />
-            <span>Tasks</span>
-          </Link>
-          <Link id="calendar" to="/calendar">
-            <FaCalendarCheckO />
-            <span>Calendar</span>
-          </Link>
-          <Link id="social" to="/social">
-            <FaFacebookOfficial />
-            <span>Social</span>
-          </Link>
-          <Link id="register" to="/signup">
-            <FaChild />
-            <span>Register</span>
-          </Link>
-          <Link id="login" to="/signin">
-            <FaSignIn />
-            <span>Login</span>
-          </Link>
-        </BurgerMenu>
-      </div>
+  handleClose(name) {
+    this.setState(
+      {open: false }
     );
+  }
+
+  handleLogOut() {
+    this.setState({
+      open: false
+    });
+    Meteor.logout();
   }
 
   render() {
-    return this.props.user
-      ? this.renderLoggedIn()
-      : this.renderLoggedOut();
+    const { user } = this.props;
+    let email = "";
+    if (user)
+      email = user.emails[0].address;
+
+    switch (this.props.path) {
+      case "/":
+        this.title = "Tasks";
+        break;
+      case "/calendar":
+        this.title = "Calendar";
+        break;
+      case "/social":
+        this.title = "Social";
+        break;
+      case "/signin":
+        this.title = "Sign In";
+        break;
+      case "/signup":
+        this.title = "Register";
+        break;
+      case "/forgotpassword":
+        this.title = "Forgot Password";
+        break;
+      default:
+        if (this.props.path.includes("reset-password"))
+          this.title = "Reset Password";
+        break;
+    }
+
+    return (
+      <div>
+        <AppBar
+          title={this.title}
+          onLeftIconButtonTouchTap={this.handleToggle.bind(this)}
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+        <Drawer
+          docked={false}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <MenuItem
+            linkButton
+            containerElement={<Link id="tasks" to="/" />}
+            onTouchTap={this.handleClose.bind(this)}
+          >
+            Tasks
+          </MenuItem>
+          <MenuItem
+            linkButton
+            containerElement={<Link id="calendar" to="/calendar" />}
+            onTouchTap={this.handleClose.bind(this)}
+          >
+            Calendar
+          </MenuItem>
+          <MenuItem
+            linkButton
+            containerElement={<Link id="social" to="/social" />}
+            onTouchTap={this.handleClose.bind(this)}
+          >
+            Social
+          </MenuItem>
+          { !user ?
+            <MenuItem
+              linkButton
+              containerElement={<Link to="/signup" />}
+              onTouchTap={this.handleClose.bind(this)}
+              id="register"
+              style={{
+                  position: 'absolute'
+              }}
+            >
+              Register
+            </MenuItem> : ''
+          }
+          { !user ?
+            < MenuItem
+              linkButton
+              containerElement={<Link to="/signin" />}
+              onTouchTap={this.handleClose.bind(this)}
+              id="login"
+              style={{
+                position: 'absolute'
+              }}
+            >
+              Sign In
+            </MenuItem> : ''
+          }
+          { user ?
+            < MenuItem
+              disabled
+              id="user-email"
+              style={{
+                position: 'absolute',
+                lineHeight: '36px'
+              }}
+            >
+              Signed in as:<br/>{email}
+            </MenuItem> : ''
+          }
+          { user ?
+            < MenuItem
+              linkButton
+              containerElement={<Link to="/" />}
+              onTouchTap={this.handleLogOut.bind(this)}
+              id="logout"
+              style={{
+                position: 'absolute'
+              }}
+            >
+              Sign Out
+            </MenuItem> : ''
+          }
+        </Drawer>
+      </div>
+    );
   }
+  
 }
 
 Menu.propTypes = {
   user: PropTypes.object,
-  logout: PropTypes.func.isRequired,
 };
