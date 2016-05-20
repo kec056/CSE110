978aca3923a57bad5d63810bd6ce223e3f7637ce
Adam Liu <alparadoxiz@gmail.com>
2016-05-20 16:12:37 -0700

    feat(tasklist) The form to add a new task is implemented along with drop down menus to customize the task

--- app/imports/ui/AddTaskButton.jsx
+++ app/imports/ui/AddTaskButton.jsx
@@ -5,6 +5,8 @@ import ContentAdd from 'material-ui/svg-icons/content/add';
 import FlatButton from 'material-ui/FlatButton';
 import Dialog from 'material-ui/Dialog';
 import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 
 import { Tasks } from '../api/Tasks.js';
 const styles = {
@@ -17,24 +19,35 @@ const styles = {
     height: 51,
     padding: 0,
   },
+  dropMenu:{
+    width: '100%',
+  }
 }
+
 export default class AddTaskButton extends React.Component {
   constructor(props){
     super(props);
     this.state ={
-      addMenuOpen: false,
+      newTaskPrompt: false,
+      timeMenu: 1,
+      priorityMenu: 1,
     };
   }
 
   //submit task
   handleSubmit(event) {
     const text = this.refs.textFieldValue.getValue();
+    const time = this.state.timeMenu;
+    const priority = this.state.priorityMenu;
 
     if (text != ''){
       Tasks.insert({
         text,
+        time,
+        priority,
         createdAt: new Date(),
       });
+      this.setState({newTaskPrompt:false});
     }
   }
 
@@ -43,7 +56,15 @@ export default class AddTaskButton extends React.Component {
       <IconButton 
         iconStyle={styles.icon} 
         style={styles.frame}
-        onFocus={()=>{this.setState({addMenuOpen:true});}}
+        onFocus={
+          ()=>{
+            this.setState({
+              newTaskPrompt:true, 
+              timeMenu: 1, 
+              priorityMenu: 1
+            });
+          }
+        }
       >
         <ContentAdd />
       </IconButton>
@@ -58,17 +79,16 @@ export default class AddTaskButton extends React.Component {
         
         onTouchTap={
           ()=>{
-            this.setState({addMenuOpen:false});
+            this.setState({newTaskPrompt:false});
           }
         }
       />,
       <FlatButton
         label="Add"
         primary={true}
-        
+        keyboardFocused={true}   
         onTouchTap={
           ()=>{
-            this.setState({addMenuOpen:false});
             this.handleSubmit(this);
           }
         }
@@ -76,21 +96,46 @@ export default class AddTaskButton extends React.Component {
     ];
     return(
       <Dialog
+        title="New Task"
         actions={actionChoice}
         modal={true}
-        open={this.state.addMenuOpen}
-        
+        open={this.state.newTaskPrompt}
+        repositionOnUpdate={false}
         onRequestClose={
           ()=>{
-            this.setState({addMenuOpen:false});
+            this.setState({newTaskPrompt:false});
           }
         }
       >
         <TextField 
-          hintText="Enter a new Task"
+          hintText="Title"
           fullWidth={true}
           ref="textFieldValue"
         />
+        <SelectField
+          value={this.state.timeMenu}
+          onChange={(event, index, value)=>{this.setState({timeMenu: value});}}
+          autoWidth={true}
+          style={styles.dropMenu}
+        >
+          <MenuItem value={1} primaryText="Default Time" />
+          <MenuItem value={2} primaryText="Morning" />
+          <MenuItem value={3} primaryText="Afternoon"/>
+          <MenuItem value={4} primaryText="Evening" />
+          <MenuItem value={5} primaryText="Night" />
+        </SelectField>
+
+        <SelectField
+          value={this.state.priorityMenu}
+          onChange={(event, index, value)=>{this.setState({priorityMenu: value});}}
+          autoWidth={true}
+          style={styles.dropMenu}
+        >
+          <MenuItem value={1} primaryText="Default Priority" />
+          <MenuItem value={2} primaryText="Low Priority" />
+          <MenuItem value={3} primaryText="Medium Priority" />
+          <MenuItem value={4} primaryText="High Priority" />
+        </SelectField>
       </Dialog>
     )
   }
