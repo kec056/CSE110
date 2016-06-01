ee6bdf3ad7cdab859b11704ae6c18776e0389480
Kevin Chen <kec056@ucsd.edu>
2016-06-01 02:33:05 -0700

    feat(Accounts): Email is now working

--- app/server/main.js
+++ app/server/main.js
@@ -3,6 +3,7 @@ import '../imports/api/Tasks.js';
 import '../imports/startup/server';
 
 Meteor.users.deny({ update: function () { return true; } });
-// Meteor.startup(() => {
-//   // code to run on server at startup
-// });
+Meteor.startup(() => {
+  // code to run on server at startup
+  // process.env.MAIL_URL = "smtp://postmaster%40sandbox9314ef87963f47ddadfcdbe3daa3da39.mailgun.org:4a21012b13c903dfb3b28ebf764ff344@smtp.mailgun.org:587";
+});
