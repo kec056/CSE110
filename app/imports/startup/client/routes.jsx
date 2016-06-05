e19815f55189c0ddb2484cdbb98b07955274688f
Spencer Wilson <ssw021@ucsd.edu>
2016-06-05 01:07:14 -0700

    fix(routes): Remove references to Settings

--- app/imports/startup/client/routes.jsx
+++ app/imports/startup/client/routes.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 // route components
-import Tasklist from '../../ui/Tasklist.jsx';
 import App from '../../ui/App.jsx';
+import Tasklist from '../../ui/Tasklist.jsx';
 import Calendar from '../../ui/Calendar.jsx';
+
 import AboutUsPage from '../../ui/AboutUsPage.jsx';
 import SignUpPage from '../../ui/SignUpPage.jsx';
 import SignInPage from '../../ui/SignInPage.jsx';
@@ -14,15 +15,15 @@ import HelpCenterPage from '../../ui/HelpCenterPage.jsx';
 
 export const renderRoutes = () => (
   <Router history={browserHistory}>
-    <Route component={App}>
-      <Route path="/" component={Tasklist} />
-      <Route path="/calendar" component={Calendar} />
-      <Route path="/aboutus" component={AboutUsPage} />
-      <Route path="/signup" component={SignUpPage} />
-      <Route path="/signin" component={SignInPage} />
-      <Route path="/forgotpassword" component={ForgotPasswordPage} />
-      <Route path="/reset-password/:token" component={ResetPasswordPage} />
-      <Route path="/helpcenter" component={HelpCenterPage} />
+    <Route path="/" component={App}>
+      <IndexRoute component={Tasklist} />
+      <Route path="calendar" component={Calendar} />
+      <Route path="aboutus" component={AboutUsPage} />
+      <Route path="signup" component={SignUpPage} />
+      <Route path="signin" component={SignInPage} />
+      <Route path="forgotpassword" component={ForgotPasswordPage} />
+      <Route path="reset-password/:token" component={ResetPasswordPage} />
+      <Route path="helpcenter" component={HelpCenterPage} />
     </Route>
   </Router>
 );
